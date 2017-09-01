b8d3802a58917b609f9c6d36e071bb4daa22afbf
cerico <garethrobertlee@gmail.com>
2017-09-01 14:17:00 +0100

    update index

--- src/js/index.js
+++ src/js/index.js
@@ -1,16 +1,28 @@
 import { Delph } from './delph'
 import { routes } from './routes'
+
 export class Index {
+
     constructor(){      
-      let header = document.getElementById('header')
-      let main = document.getElementById('main')
-      header.innerHTML = "click"
-      header.addEventListener('click', () => {this.load('glossop')})
+      var main = document.getElementById('main')
       this.delph = new Delph(routes,main)
+      this.makeHeader(routes)
     }
+
+    makeHeader(routes){
+      let el = document.getElementById('header')
+      Object.keys(routes).map(route => {
+        var e = document.createElement("li");
+        e.innerHTML = route
+        e.addEventListener('click', () => {this.load(route)})
+        el.appendChild(e);
+      })
+    }
+ 
     load(link){
       this.delph.load(link)
     }
+    
 };
 document.addEventListener('DOMContentLoaded', () => {
   new Index()
