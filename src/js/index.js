7195bbe74b3b9e56ec27b3edc8947df7d2ec7293
cerico <garethrobertlee@gmail.com>
2017-09-01 12:53:21 +0100

    replace var

--- src/js/index.js
+++ src/js/index.js
@@ -4,7 +4,7 @@ import { routes } from './routes'
 export class Index {
 
     constructor(){      
-      var main = document.getElementById('main')
+      let main = document.getElementById('main')
       this.delph = new Delph(routes,main)
       this.makeHeader(routes)
     }
@@ -12,7 +12,7 @@ export class Index {
     makeHeader(routes){
       let el = document.getElementById('header')
       Object.keys(routes).map(route => {
-        var e = document.createElement("li");
+        let e = document.createElement("li");
         e.innerHTML = route
         e.addEventListener('click', () => {this.load(route)})
         el.appendChild(e);
