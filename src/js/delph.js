a842208ead8efe7943965f8cc10397af0b95642c
cerico <garethrobertlee@gmail.com>
2017-09-02 18:11:33 +0100

    404

--- src/js/delph.js
+++ src/js/delph.js
@@ -8,7 +8,12 @@ export class Delph {
 
     load(page){
         history.pushState({ page}, null, `/${page}`);
+        console.log(page)
         let route =  this.routes[page];
-        route.load().then(r => route.show(this.el));
+        if (route) {
+            route.load().then(r => route.show(this.el));
+        } else {
+            this.el.innerHTML = "no page found"
+        }
     }
 }
