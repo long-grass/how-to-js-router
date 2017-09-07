f7009dc77a87312f903c8c550541d2097de9d112
cerico <garethrobertlee@gmail.com>
2017-09-07 13:09:42 +0100

    components one

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,9 +1,15 @@
-import { Page } from './page';
+import { MaltonComponent } from './components/malton'
+import { GlossopComponent } from './components/glossop'
+import { KendalComponent } from './components/kendal'
+import { HerefordComponent } from './components/hereford'
+import { DefaultComponent } from './components/default'
+
+
 
 export const routes = {
-    glossop: new Page('glossop'),
-    kendal:  new Page('kendal'),
-    hereford:  new Page('hereford'),
-    malton:  new Page('malton'),
-    '' : new Page('default')
+    glossop: GlossopComponent,
+    kendal:  KendalComponent,
+    hereford:  HerefordComponent,
+    malton:   MaltonComponent,
+    '' : DefaultComponent
 }
