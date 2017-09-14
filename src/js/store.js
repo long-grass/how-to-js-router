00b8720146bf159d44bd7d057be569cef9db0b47
cerico <garethrobertlee@gmail.com>
2017-09-14 14:48:28 +0100

    custom event for state change

--- src/js/store.js
+++ src/js/store.js
@@ -1,23 +1,21 @@
 export const store =  {
-    counter: 0
+  counter: 0
 };
 
 document.addEventListener('action', (e) => {
-    console.log(e)
-    console.log(store)
-    let result = counter(store, e.detail);
-    console.log(result)
+  let result = counter(store, e.detail);
+  document.dispatchEvent(new CustomEvent('stateChange'));
 })
 
 function counter(state, action) {
-    console.log(state,action)
+  console.log(state,action)
 	switch (action) {
-        case 'increaseCount':
-            const newState = Object.assign(state, {
+    case 'increaseCount':
+      const newState = Object.assign(state, {
 				counter: state.counter + 1
 			});
 			return newState
 		default:
 			return state;
-    }
+  }
 }
