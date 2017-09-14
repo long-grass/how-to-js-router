42ba31f0cb4168dd1b62df0dfc0e37eae78285e0
cerico <garethrobertlee@gmail.com>
2017-09-14 14:34:52 +0100

    first reducer

--- src/js/store.js
+++ src/js/store.js
@@ -2,4 +2,22 @@ export const store =  {
     counter: 0
 };
 
-document.addEventListener('action', (e) => console.log(e))
+document.addEventListener('action', (e) => {
+    console.log(e)
+    console.log(store)
+    let result = counter(store, e.detail);
+    console.log(result)
+})
+
+function counter(state, action) {
+    console.log(state,action)
+	switch (action) {
+        case 'increaseCount':
+            const newState = Object.assign(state, {
+				counter: state.counter + 1
+			});
+			return newState
+		default:
+			return state;
+    }
+}
