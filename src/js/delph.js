3fbf85d47e166b4af6f4f8d7c68c4eb0403f98cb
cerico <garethrobertlee@gmail.com>
2017-09-28 22:56:02 -0400

    router subscribed to store

--- src/js/delph.js
+++ src/js/delph.js
@@ -8,36 +8,13 @@ export class Delph {
     document.body.appendChild(this.routerOutlet)
     config.store.subscribe(this.render.bind(this));
     config.store.dispatch(loadRoute({path:config.path}))
-    this.handleBackButton()
   }
-    
-  handleBackButton(){    
-    window.onpopstate = (event) => {  
-      let content = "";
-      if (event.state) {
-        content = event.state.page;
-        this.load(content, true)
-      }
+  
+  render(page){
+    while (this.routerOutlet.firstChild) {
+      this.routerOutlet.removeChild(this.routerOutlet.firstChild);
     }
-  }
-
-  inject(component) {
-    const routerOutlet = this.routerOutlet
-    while (routerOutlet.firstChild) {
-      routerOutlet.removeChild(routerOutlet.firstChild);
-    }
-    routerOutlet.appendChild(new component);
-  }
-
-  render(page, backButtonUsed){
-    if (!backButtonUsed){
-      history.pushState({ page}, null, `/${page.path}`);
-    }
-    let route =  this.routes[page.path];
-    if (route) {
-      this.inject(route)
-    } else {
-      this.routerOutlet.innerHTML = "no page found"
-    }
+    this.routerOutlet.appendChild(new this.routes[page.path])
   }
 }
+
