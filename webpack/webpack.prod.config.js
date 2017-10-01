86ddbf915e92f950494159b398eab5ce43450df7
cerico <garethrobertlee@gmail.com>
2017-10-01 16:19:33 -0400

    add babel to prod webpack

--- webpack/webpack.prod.config.js
+++ webpack/webpack.prod.config.js
@@ -6,6 +6,18 @@ module.exports = options => {
     output: {
       path: path.join(__dirname, '../', '/dist'),
       filename: 'bundle.js',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            presets:[ 'stage-2' ]
+          }
+        }
+      ]
     }
   }
 } 
