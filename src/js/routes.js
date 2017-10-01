0bcddf149fcb95bb19e553b1e2f1cb00ea6227b9
cerico <garethrobertlee@gmail.com>
2017-10-01 18:11:56 -0400

    nestedRoutes

--- src/js/routes.js
+++ src/js/routes.js
@@ -2,8 +2,8 @@ import { GlossopComponent } from './components/glossop'
 import { KendalComponent } from './components/kendal'
 import { DefaultComponent } from './components/default'
 
-export const routes = {
-    glossop: GlossopComponent,
-    kendal:  KendalComponent,
-    '' : DefaultComponent
-}
+export const routes = [
+    {title:'glossop', path: 'glossop/nested', component:GlossopComponent},
+    {title: 'kendal', path: 'kendal/refactored', component: KendalComponent},
+    {title: 'index', path: '', component: DefaultComponent}
+]
