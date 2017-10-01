55171c54bea93d29025dd9e5a823562f570019b5
cerico <garethrobertlee@gmail.com>
2017-10-01 13:47:12 -0400

    readd back button

--- src/js/delph.js
+++ src/js/delph.js
@@ -8,6 +8,18 @@ export class Delph {
     document.body.appendChild(this.routerOutlet)
     config.store.subscribe(this.render.bind(this));
     config.store.dispatch(loadRoute({path:config.path}))
+    this.handleBackButton(config.store)
+  }
+
+  handleBackButton = (store) => {
+ 
+    window.onpopstate = (event) => {
+      let content = "";
+      if (event.state) {
+        content = event.state.page;
+        store.dispatch(loadRoute({path:content,back:true}))
+      }
+    }
   }
   
   render(previousState,state){
